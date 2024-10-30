a3b96cb63a5502fc5dcb8c17e633c358b81b3418
Om Parihar <141987998+salvatoreOm@users.noreply.github.com>
2024-10-30 14:00:09 +0530

    Update GameScene.swift

--- AngryBirdClone/GameScene.swift
+++ AngryBirdClone/GameScene.swift
@@ -1,10 +1,6 @@
 //
 //  GameScene.swift
 //  AngryBirdClone
-//
-//  Created by Atil Samancioglu on 12.08.2019.
-//  Copyright © 2019 Atil Samancioglu. All rights reserved.
-//
 
 import SpriteKit
 import GameplayKit
